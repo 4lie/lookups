7a7c12de02535c2e6df89e8fc74ce47fd1971182
Parsa Eskandarnejad <32741675+parsaaes@users.noreply.github.com>
2020-09-12 13:40:56 +0430

    Add indexing by S2 (#2)

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/golang/geo v0.0.0-20200730024412-e86565bf3f35
+	github.com/stretchr/testify v1.6.1
 	github.com/twpayne/go-geom v1.3.6
 )
---
+++ index/s2.go
@@ -0,0 +1,25 @@
+package index
+
+import (
+	"github.com/golang/geo/s2"
+)
+
+type S2Hash struct {
+	level int
+}
+
+func NewS2Hash(level int) *S2Hash {
+	return &S2Hash{level: level}
+}
+
+func (s *S2Hash) Cover(polygon *s2.Polygon) []string {
+	ids := s2.SimpleRegionCovering(polygon, polygon.Edge(0).V0, s.level)
+
+	out := make([]string, 0, len(ids))
+
+	for _, id := range ids {
+		out = append(out, id.ToToken())
+	}
+
+	return out
+}
