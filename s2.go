5d550b9c8c08e7ae5f3ee8f8cef9d84898091562
Parsa Eskandarnejad <parsaaes@gmail.com>
2020-09-12 16:01:58 +0430

    Improve covering method

--- s2.go
+++ s2.go
@@ -12,7 +12,11 @@ func NewS2Hash(level int) S2Hash {
 	return S2Hash{level: level}
 }
 
-func (s *S2Hash) Cover(polygon *s2.Polygon) []string {
+func (s S2Hash) Cover(polygon *s2.Polygon) []string {
+	if polygon.NumEdges() < 1 {
+		return []string{}
+	}
+
 	ids := s2.SimpleRegionCovering(polygon, polygon.Edge(0).V0, s.level)
 
 	out := make([]string, 0, len(ids))
